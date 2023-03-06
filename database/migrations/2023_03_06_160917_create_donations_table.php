0d7e535eafdf6512130e9f946d8bd7c305571e64
Gede Ardi Pratama <67818281+Pratamaardhy@users.noreply.github.com>
2023-03-06 23:43:35 +0700

    Add files via upload

---
+++ database/migrations/2023_03_06_160917_create_donations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users','id');
+            $table->foreignId('campaign_id')->constrained('campaigns','id');
+            $table->string('name');
+            $table->fload('amount_donations');
+            $table->text('hope')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
