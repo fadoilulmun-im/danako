823e61e6b2971beee8f5fff90c9c8017698afee0
Pratamaardhy <gedeardi0702@gmail.com>
2023-03-16 09:57:12 +0700

    migrate from develop

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+<<<<<<< HEAD
+    return view('admin.page.index');
+=======
+    return view('welcome');
+>>>>>>> 6dea91e6c7a2160a9d1a8b56c087f94100108fc4
 });
