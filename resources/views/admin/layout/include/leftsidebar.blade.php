019499f90540de084a8282849fd273d6fa13af23
Khodijah Aulia Rahma <83078030+kodijeee@users.noreply.github.com>
2023-05-04 11:19:12 +0700

    ngerapihin menu

--- resources/views/admin/layout/include/leftsidebar.blade.php
+++ resources/views/admin/layout/include/leftsidebar.blade.php
@@ -38,14 +38,14 @@
                                 <a href="/admin/campaigns">Campaigns</a>
                             </li>
                             <li>
-                                <a href="#">Withdraw Funds</a>
+                                <a href="#">Funds Disbursement</a>
                             </li>
                         </ul>
                     </div>
                 </li>
                 <li>
                     <a href="#donations" data-bs-toggle="collapse">
-                        <i class="mdi mdi-wallet-outline"></i>
+                        <i class="dripicons-wallet"></i>
                         <span> Donations </span>
                         <span class="menu-arrow"></span>
                     </a>
@@ -61,41 +61,18 @@
                     </div>
                 </li>
                 <li>
-                    <a href="#sidebarTasks" data-bs-toggle="collapse">
-                        <i class="mdi mdi-clipboard-outline"></i>
-                        <span> Tasks </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <div class="collapse" id="sidebarTasks">
-                        <ul class="nav-second-level">
-                            <li>
-                                <a href="task-kanban-board.html">Kanban Board</a>
-                            </li>
-                            <li>
-                                <a href="task-details.html">Details</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <a href="apps-projects.html">
-                        <i class="mdi mdi-briefcase-variant-outline"></i>
-                        <span> Projects </span>
-                    </a>
-                </li>
-                <li>
                     <a href="#contacts" data-bs-toggle="collapse">
                         <i class="mdi mdi-book-open-page-variant-outline"></i>
-                        <span> Contacts </span>
+                        <span> Report </span>
                         <span class="menu-arrow"></span>
                     </a>
                     <div class="collapse" id="contacts">
                         <ul class="nav-second-level">
                             <li>
-                                <a href="contacts-list.html">Members List</a>
+                                <a href="#">Report 1</a>
                             </li>
                             <li>
-                                <a href="contacts-profile.html">Profile</a>
+                                <a href="#">Report 2</a>
                             </li>
                         </ul>
                     </div>
